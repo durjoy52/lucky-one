4056308abff223765c971a3aa767dd81ecd20439
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-03-26 02:00:13 +0600

    header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='bg-success p-4 mb-3'>
-             <h1 className='text-center'>Watch Store ⌚</h1>
+        <div className='bg-success p-2 mb-3 text-center'>
+             <h1>Watch Store ⌚</h1>
+             <h2 className='text-white'>Choose 4 watches</h2>
         </div>
     );
 };
