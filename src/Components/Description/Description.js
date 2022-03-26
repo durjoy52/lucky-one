a2d7e0951f2bbba6f486d1c78de17a45176ca1cb
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-03-26 10:15:20 +0600

    description fixed

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -6,7 +6,7 @@ const Description = () => {
             <div>
                 <h2>How react works ?</h2>
                 <p>
-                React is the most popular javascript framework or library whatever you call it.React actually create tree for user.React create a virtual dom .Virtual dom compare to browser actual dom and changes only which statement is changes.
+                React is the most popular javascript framework or library whatever you call it. React creates a virtual dom . When rendering actual dom react creates the same dom. That's called virtual dom. Virtual dom compares between her old virtual dom and changes only which state or data has been changed.
                 </p>
             </div>
             <div>
@@ -14,7 +14,7 @@ const Description = () => {
             <p>
                 Props are used to communicate between components.By using props as arguments you can pass data one component to other component.Props is unidirectional.Props is immutable.
             </p>
-            <p>state can be used for rendering dynamic changes with her components.State cannot make component reusable.State cannot be accessed by child component</p>
+            <p>state can be used for rendering dynamic changes with her components.State cannot make component reusable.State cannot be accessed by child components.</p>
             </div>
         </div>
     );
